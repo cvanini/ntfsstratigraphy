25cd53c04edd380857f77852fc8435220450b974
cvanini <celine.vanini@unil.ch>
2022-04-13 19:46:06 +0200

    bloup

---
+++ src/fsparser/ressources/dict.py
@@ -0,0 +1,197 @@
+
+
+header = {
+    'FILE/BAAD': '',
+    'Entry use counter': '',
+    '$LogFile sequence number (LSN)': '',
+    'First attribute offset': '',
+    'Allocation flag': '',
+    'Allocation flag (verbose)': '',
+    'Entry logical size': '',
+    'Entry physical size': '',
+    'Entry number': ''
+}
+
+standard = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': '',
+    'Creation time': '',
+    'Modification time': '',
+    'Entry modification time': '',
+    'Last accessed time': ''
+}
+
+file_type = {
+    0: 'Read-only',
+    1: 'Hidden File',
+    2: 'System File',
+    3: 'None',
+    4: 'None',
+    5: 'Archive',
+    6: 'Device',
+    7: 'Normal',
+    8: 'Temporary',
+    9: 'Sparse File',
+    10: 'Reparse Point',
+    11: 'Compressed File',
+    12: 'Offline',
+    13: 'Not Content Indexed',
+    14: 'Encrypted File',
+    15: 'None'
+}
+
+attributes_ID = {
+    0: 'Unknown',
+    16: '$STANDARD_INFORMATION',
+    32: '$ATTRIBUTE_LIST',
+    48: '$FILE_NAME',
+    64: '$OBJECT_ID',
+    80: '$SECURITY_DESCRIPTOR',
+    96: '$VOLUME_NAME',
+    112: '$VOLUME_INFORMATION',
+    128: '$DATA',
+    144: '$INDEX_ROOT',
+    160: '$INDEX_ALLOCATION',
+    176: '$BITMAP',
+    192: '$REPARSE_POINT',
+    208: '$EA_INFORMATION',
+    224: '$EA',
+    256: '$LOGGED_UTILITY_STREAM'
+}
+
+
+
+
+attribute_list = {
+
+}
+
+filename = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': '',
+    'Parent entry number': '',
+    'Creation time': '',
+    'Modification time': '',
+    'Entry modification time': '',
+    'Last accessed time': '',
+    'Filename length': '',
+    'Filename': ''
+}
+
+volume_name = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+volume_information = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+security_descriptor = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+object_id = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+data = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+index_node = {
+
+}
+
+index_entry = {
+
+}
+
+index_root = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+index_allocation = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+bitmap = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+reparse_point = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
+
+logged_utility_stream = {
+    'Attribute first offset': '',
+    'Attribute size': '',
+    'Resident': '',
+    'Content start offset': '',
+    'Resident (verbose)': '',
+    'Content size': '',
+    'Content start offset (resident)': ''
+}
